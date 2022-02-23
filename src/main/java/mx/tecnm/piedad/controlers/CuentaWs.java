12e5916a7df33f2083874e0a1166de67c3adff28
JuanG7u7 <juan14yure@gmail.com>
2022-02-23 08:56:25 -0600

    Primer cambio del proyecto

--- src/main/java/mx/tecnm/piedad/controlers/CuentaWs.java
+++ src/main/java/mx/tecnm/piedad/controlers/CuentaWs.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+// un cambio xd
 import mx.tecnm.piedad.dao.CuentaJDBC;
 import mx.tecnm.piedad.models.Cuenta;
 
